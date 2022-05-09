237e4127fa244a91ecc368f3fafab68ee56ea22c
Nirajray <nirajray5555@gmail.com>
2022-05-09 18:01:03 +0530

    my code

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 
 
-router.post("/", collegeController.createCollege)
-router.post("/functionup/interns", internController.createIntern)
-router.get("/functionup/collegeDetails", internController.getList)
+
 
 module.exports = router
